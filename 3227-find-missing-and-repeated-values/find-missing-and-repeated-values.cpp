1e3addd0731e896e24b328b50421645353fbe9e5
Sanish Santosh Dalvi <sanishdalvi@gmail.com>
2026-08-02 13:05:15 +0530

    Time: 20 ms (17.84%) | Memory: 31.9 MB (5.26%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int a,b,asum=0;
+        int a = 0;
         unordered_set<int> set;
-        int n=grid.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                asum+=grid[i][j];
-                if(set.find(grid[i][j])!=set.end()){
-                    a=grid[i][j];
+        int n = grid.size();
+        int sum = (n * n) * ((n * n) + 1) / 2;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sum -= grid[i][j];
+                if (set.find(grid[i][j]) != set.end()) {
+                    a = grid[i][j];
                 }
                 set.insert(grid[i][j]);
             }
         }
-        n*=n;
-        int sum=n*(n+1)/2;
-        b=sum -(asum-a);
-        return {a,b} ;
+        return {a, sum + a};
     }
 };
