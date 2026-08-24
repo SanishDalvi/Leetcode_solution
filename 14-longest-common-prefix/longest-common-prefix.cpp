ea06656b963adf8dfa18b542ee31db5e563fc725
Sanish Santosh Dalvi <sanishdalvi@gmail.com>
2026-08-24 21:07:29 +0530

    Time: 7 ms (3.70%) | Memory: 18.1 MB (5.25%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,17 +3,14 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string ret = "";
         int i = 0;
-        while (true) {
-            if (strs[0][i] == '\0')
-                return ret;
-            char c = strs[0][i];
-            for (string s : strs) {
-                if (s[i] == '\0' || s[i] != c)
-                    return ret;
-            }
-            ret += c;
-            i++;
+        while(i<strs[0].length()){
+        char c=strs[0][i];
+        for(string s:strs){
+            if(s[i]=='\0' || s[i]!=c) return ret;
         }
+        ret+=c;
+        i++;
+    }
         return ret;
     }
 };
