c690b17fd3f78f069f30399b08469c8520d07948
Sanish Santosh Dalvi <sanishdalvi@gmail.com>
2026-09-03 20:35:23 +0530

    Time: 3 ms (14.58%) | Memory: 23.9 MB (19.62%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0 || n==1){
+            return;
+        }
+        int idx=0;
+        for(int i=0;i<n;i++){
+            if (nums[i]!=0){
+                nums[idx++]=nums[i];
+            }
+        }
+        for(int i=idx;i<n;i++){
+            nums[i]=0;
+        }
+        return;
+    }
+};
