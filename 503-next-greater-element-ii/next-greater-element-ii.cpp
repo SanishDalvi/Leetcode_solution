9883bec30f339272df1225cc4b1bf5ae65fdf150
Sanish Santosh Dalvi <sanishdalvi@gmail.com>
2026-09-06 21:33:45 +0530

    Time: 3 ms (74.72%) | Memory: 28.6 MB (39.12%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -5,12 +5,11 @@ public:
     vector<int> ans(n,-1);
     stack<int> st;
     for(int i=2*n-1;i>=0;i--){
-        int idx=i%n;
-        while(!st.empty() && st.top()<=nums[idx]){
+        while(!st.empty() && st.top()<=nums[i%n]){
             st.pop();
         }
-        if(!st.empty()) ans[idx]=st.top();
-        st.push(nums[idx]);
+        if(!st.empty()) ans[i%n]=st.top();
+        st.push(nums[i%n]);
     }        
     return ans;
     }
