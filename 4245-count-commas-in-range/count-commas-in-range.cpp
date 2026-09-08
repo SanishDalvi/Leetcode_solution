c99d84137676e75cfd719421a204696c3b55daa8
Sanish Santosh Dalvi <sanishdalvi@gmail.com>
2026-09-08 19:22:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (15.51%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int digits = log10(n) + 1;
-        if (digits == 6) {
-            return 99001;
-        } else {
-            if(n - 999 >=0) return n-999;
-            else return 0;
-        }
+         if (n < 1000)
+            return 0;
+
+        return (n - 1000) + 1;
     }
 };
